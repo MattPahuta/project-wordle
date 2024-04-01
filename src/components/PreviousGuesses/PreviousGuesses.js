dc29c0ae1e167a088aa01c7957f398a91eaa407a
Matt Pahuta <mhpahuta@gmail.com>
2024-04-01 14:51:00 -0700

    Complete exercise 3, add guess grid slots

--- src/components/PreviousGuesses/PreviousGuesses.js
+++ src/components/PreviousGuesses/PreviousGuesses.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Guess from '../Guess/Guess';
+import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function PreviousGuesses({guesses}) {
   return (
-    <div class="guess-results">
-      {guesses.map((guess, index) => (
-        <p className='guess' key={index}>
-          {guess}
-        </p>
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        // <p className='guess' key={index}>
+        //   {guess}
+        // </p>
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   )
